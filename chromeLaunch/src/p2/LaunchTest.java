99de451e619138325cb07442b98aff7ce0212f03
user <user@DESKTOP-AR44GUJ>
2019-04-12 11:03:29 +0530

    commit

---
+++ chromeLaunch/src/p2/LaunchTest.java
@@ -0,0 +1,18 @@
+package p2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchTest
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+		String url="https://www.facebook.com";
+		WebDriver driver = new ChromeDriver();
+		driver.get(url);
+		Thread.sleep(2000);
+		driver.close();
+		
+	}
+
+}
